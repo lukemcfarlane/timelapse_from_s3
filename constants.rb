c065991ca0d93b4b132db3bfda55d7b5e04208ac
Luke McFarlane <luke.mcfarlane@trineo.co.nz>
2017-10-04 14:14:57 +1300

    Make 720p mp4 video (Quicktime compatible)

--- constants.rb
+++ constants.rb
@@ -3,7 +3,7 @@ require 'time'
 
 FRAMERATE=25
 FRAMES_OUTPUT_DIR = File.join(File.dirname(__FILE__), 'frames').freeze
-OUTPUT_FILENAME = 'output.avi'
+OUTPUT_FILENAME = 'output.mp4'
 DATE_RANGE = DateTime.new(2017, 9, 1)..DateTime.now
 TIME_RANGE = Time.parse('07:00')..Time.parse('18:00')
 MAX_INTERVAL_MINS = 60 * 6
