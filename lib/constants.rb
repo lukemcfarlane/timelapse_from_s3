0c2614c21c9313af1126e4d2b48a79c12ebf0efe
Luke McFarlane <luke.mcfarlane@trineo.co.nz>
2017-12-14 11:29:46 +1300

    Add ability to select every nth valid photo.

--- lib/constants.rb
+++ lib/constants.rb
@@ -6,4 +6,5 @@ PHOTOS_OUTPUT_DIR = File.join(File.dirname(__FILE__), '..', 'photos').freeze
 FRAMES_TEMP_DIR = File.join(File.dirname(__FILE__), '..', 'frames_temp').freeze
 OUTPUT_FILENAME = File.join(File.dirname(__FILE__), '..', 'output.mp4').freeze
 
-EXPOSURE_RANGE = 25000..45000
+SELECT_EVERY=3
+EXPOSURE_RANGE = 25000..41000
--- lib/make_frames.rb
+++ lib/make_frames.rb
@@ -11,7 +11,13 @@ photo_filenames = Dir.glob(File.join(PHOTOS_OUTPUT_DIR, '*'))
 photos = photo_filenames.map { |filename| Photo.new(filename) }
 valid_photos = photos.select &:valid?
 
-puts "Selected #{valid_photos.count} out of #{photos.count} photos"
+puts "Removed #{photos.count - valid_photos.count} photos that were outside exposure range"
+
+filtered_photos = valid_photos.select.with_index do |_, i|
+  i % SELECT_EVERY == 0
+end
+
+puts "Selected total of #{filtered_photos.count} photos"
 
 if Dir.exist? FRAMES_TEMP_DIR
   FileUtils.rm_rf Dir.glob(File.join(FRAMES_TEMP_DIR, '*'))
@@ -19,6 +25,6 @@ else
   Dir.mkdir FRAMES_TEMP_DIR
 end
 
-valid_photos.sort.each_with_index do |photo, i|
+filtered_photos.sort.each_with_index do |photo, i|
   FileUtils.cp(photo.filename, File.join(FRAMES_TEMP_DIR, "#{i}.jpg"))
 end
